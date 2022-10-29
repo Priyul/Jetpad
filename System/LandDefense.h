e0e12ea143979d6cbe76afbdde1ddf94484fd92e
Jhindosh <dharshanpillay@gmail.com>
2022-10-29 11:03:52 +0200

    ✅ Defending Files

---
+++ System/LandDefense.h
@@ -0,0 +1,14 @@
+#ifndef LANDDEFENSE_H
+#define LANDDEFENSE_H
+
+#include "Defending.h"
+
+class LandDefense: public Defending
+{
+    public:
+        void landDefense();
+        LandDefense handle();
+        ~landDefense();
+};
+
+#endif
---
+++ System/SeaDefense.cpp
@@ -0,0 +1,11 @@
+#include "SeaDefense.h"
+
+void SeaDefense::seaDefense()
+{
+
+}
+
+SeaDefense SeaDefense::handle()
+{
+    
+}

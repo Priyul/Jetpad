2b4344daefa82867d971a3dc7a1650c2e805b3fe
JKHTY <u20660546@tuks.co.za>
2022-11-06 22:45:11 +0200

    📝 Doxygen

--- System/Route.h
+++ System/Route.h
@@ -5,11 +5,44 @@
 
 class Route : public Transport{
 public:
+    /**
+     * @brief Get the Description object
+     * 
+     * @return ** std::string 
+     */
     std::string getDescription();
+    /**
+     * @brief Set the Description object
+     * 
+     * @param description 
+     * @return ** void 
+     */
     void setDescription(std::string description);
+    /**
+     * @brief Get the Money Gained object
+     * 
+     * @return ** double 
+     */
     double getMoneyGained();
+    /**
+     * @brief Set the Money Gained object
+     * 
+     * @param money 
+     * @return ** void 
+     */
     void setMoneyGained(double money);
+    /**
+     * @brief Set the Turns To Skip object
+     * 
+     * @param turns 
+     * @return ** void 
+     */
     void setTurnsToSkip(int turns);
+    /**
+     * @brief Get the Turns To Skip object
+     * 
+     * @return ** int 
+     */
     int getTurnsToSkip();
 };
 

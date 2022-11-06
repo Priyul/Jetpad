c1f73e546e6fefcb5acd4b075045e6d5c5f0fde3
JKHTY <u20660546@tuks.co.za>
2022-11-06 23:24:15 +0200

    📝 Doxygen

--- System/WarStrategy.h
+++ System/WarStrategy.h
@@ -7,6 +7,19 @@
 
 class WarStrategy{
 public:
+    /**
+     * @brief Choosing a war strategy and simulating fight between two army objects
+     * 
+     * @param ourArmy 
+     * @param AIArmy 
+     * @param playerAttackStrategy 
+     * @param CPUDefenseStrategy 
+     * @param noOfAttackingVehiclesToSend 
+     * @param noOfAttackingMajorsToSend 
+     * @param noOfAttackingSergeantsToSend 
+     * @param noOfAttackingPrivatesToSend 
+     * @return ** std::string 
+     */
     virtual std::string handle(vector<Army*> &ourArmy, vector<Army*> &AIArmy, std::string playerAttackStrategy, std::string CPUDefenseStrategy, int noOfAttackingVehiclesToSend, int noOfAttackingMajorsToSend, int noOfAttackingSergeantsToSend, int noOfAttackingPrivatesToSend) = 0;
 };
 
